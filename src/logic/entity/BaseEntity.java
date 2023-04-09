133b3a0784036572881e90f49dca7cf22bb37a82
GriszlyExe <grillmon_123@hotmail.com>
2023-04-09 19:19:42 +0700

    Test push

--- src/logic/entity/BaseEntity.java
+++ src/logic/entity/BaseEntity.java
@@ -1,5 +1,37 @@
 package logic.entity;
 
 public class BaseEntity {
+	private int hp,atk;
+	private String name;
+	
+	public BaseEntity(String name , int hp ,int atk) {
+		setHp(hp);
+		setAtk(atk);
+		setName(name);
+		
+	}
 
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
