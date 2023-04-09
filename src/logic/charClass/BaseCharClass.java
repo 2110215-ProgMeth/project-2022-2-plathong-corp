4c1b712dafea5ff57762609e2ebf290127ceb31e
GriszlyExe <grillmon_123@hotmail.com>
2023-04-09 20:03:54 +0700

    BaseEntity test

--- src/logic/charClass/BaseCharClass.java
+++ src/logic/charClass/BaseCharClass.java
@@ -1,5 +1,41 @@
 package logic.charClass;
 
-public class BaseCharClass {
+public abstract class BaseCharClass {
+	private int hp,atk;
+	private String name;
+	
+	public BaseCharClass(String name , int hp ,int atk) {
+		setHp(hp);
+		setAtk(atk);
+		setName(name);
+		
+	}
+	
+	public abstract void normalAttack();
+	public abstract void performUltmate();
+	public abstract void performBuff();
 
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
